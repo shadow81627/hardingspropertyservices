569de1fdff31bff49739977c5b11a1eefd56b1b8
Damien Robinson <damien.robinson@pocketpasta.com>
2022-02-12 12:09:49 +1000

    feat(footer): site by damien robinson

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,16 @@
 <template>
   <div
-    text="xl gray4"
+    text="gray4"
     m-5
     flex
     justify-center
     gap-3
   >
     <NuxtLink i-carbon-campsite to="/" />
-    <a i-carbon-logo-github href="https://github.com/antfu/vitesse-nuxt3" target="__blank" />
+    <!-- <a i-carbon-logo-github href="https://github.com/shadow81627/hardingspropertyservices" target="__blank" /> -->
     <DarkToggle />
+    <small>
+      <a href="https://daim.dev" target="__blank">Site by Damien Robinson</a>
+    </small>
   </div>
 </template>
