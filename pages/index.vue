9ee9461943fa5e68e1e75acf41350ce1515a6639
Damien Robinson <damien.robinson@pocketpasta.com>
2022-02-13 09:47:31 +1000

    feat(home): add list text

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
+  <div class="text-center">
     <!-- <Logos mb-6 /> -->
-    <h1>Hardings Property Services</h1>
-    <Suspense>
+    <h1 class="text-2xl font-bold leading-7 sm:text-3xl sm:truncate">
+      Hardings Property Services
+    </h1>
+
+    <ul class="list-disc">
+      <li>Property maintenance</li>
+      <li>Lawn mowing</li>
+      <li>General handyman services</li>
+      <li>Odd jobs</li>
+      <li>Flat pack assembly</li>
+    </ul>
+
+    <p>
+      Servicing the local Beaudesert and surrounding areas
+    </p>
+    <!-- <Suspense>
       <PageView />
       <template #fallback>
         <div op50 italic>
           <span animate-pulse>Loading...</span>
         </div>
       </template>
-    </Suspense>
+    </Suspense> -->
     <!-- <InputEntry /> -->
   </div>
 </template>
