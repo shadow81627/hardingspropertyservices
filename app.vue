973694f1a40075272ed8b70303647c27be04a2f5
Damien Robinson <damien.robinson@pocketpasta.com>
2022-02-13 17:01:52 +1000

    feat: structured data

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <Html lang="en-AU">
+    <Script type="application/ld+json" :children="jsonLD" />
     <div class="min-h-screen flex flex-col justify-between">
       <main class="py-20 px-10 mb-auto">
         <NuxtPage />
@@ -15,6 +16,71 @@
     </div>
   </Html>
 </template>
+
+<script setup>
+const config = useRuntimeConfig()
+const jsonLD = JSON.stringify({
+  '@context': 'https://schema.org/',
+  '@type': 'Service',
+  'serviceType': 'Handyman',
+  'provider': {
+    '@type': 'LocalBusiness',
+    'name': config.APP_NAME,
+    // 'telephone': '0487 950 940',
+    'url': 'https://hardingspropertyservices.com.au',
+    'sameAs': [
+      'https://www.instagram.com/hardingspropertyservices/',
+      'https://www.facebook.com/hardingspropertyservices',
+    ],
+  },
+  'areaServed': {
+    '@type': 'City',
+    'name': ['Beaudesert', 'Kooralbyn'],
+  },
+  'hasOfferCatalog': {
+    '@type': 'OfferCatalog',
+    'name': 'Property services',
+    'itemListElement': [
+
+      {
+        '@type': 'Offer',
+        'itemOffered': {
+          '@type': 'Service',
+          'name': 'Property maintenance',
+        },
+      },
+      {
+        '@type': 'Offer',
+        'itemOffered': {
+          '@type': 'Service',
+          'name': 'Lawn mowing',
+        },
+      },
+      {
+        '@type': 'Offer',
+        'itemOffered': {
+          '@type': 'Service',
+          'name': 'General handyman services',
+        },
+      },
+      {
+        '@type': 'Offer',
+        'itemOffered': {
+          '@type': 'Service',
+          'name': 'Odd jobs',
+        },
+      },
+      {
+        '@type': 'Offer',
+        'itemOffered': {
+          '@type': 'Service',
+          'name': 'Flat pack assembly',
+        },
+      },
+    ],
+  },
+})
+</script>
 
 <style>
 @import '~/styles/main.css';
