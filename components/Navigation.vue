89f0bf4a793f352cdb82519c66f43267aaadab4e
Damien Robinson <damien.robinson@pocketpasta.com>
2022-02-19 15:13:40 +1000

    feat: contact form (#4)
    * fix(manifest): udpate name with '
    
    * chore(deps): add formkit
    
    * feat: contact form

---
+++ components/Navigation.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="relative w-full flex flex-wrap items-center justify-between py-3">
+    <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+      <div class="container-fluid">
+            &nbsp;
+      </div>
+      <DarkToggle />
+    </div>
+  </nav>
+</template>
