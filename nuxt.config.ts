958a886e38972c02bca53739c092ad7fdc20fbe7
Damien Robinson <damien.robinson@pocketpasta.com>
2022-02-19 16:25:42 +1000

    fix: remove formkit (#6)
    * fix(manifest): udpate name with '
    
    * chore(deps): add formkit
    
    * feat: contact form
    
    * fix: remove formkit
    
    Error caused by imporper export setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfig({
   },
   modules: [
     'vue-plausible',
+    // '@formkit/nuxt',
   ],
   buildModules: [
     '@vueuse/nuxt',
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,6 @@
 <template>
-  <div class="text-center">
-    <h1 class="text-2xl font-bold leading-7 sm:text-3xl sm:truncate">
-      {{ $config.APP_NAME }}
-    </h1>
-
-    <ul class="list-none">
-      <li>Property maintenance</li>
-      <li>Lawn mowing</li>
-      <li>General handyman services</li>
-      <li>Odd jobs</li>
-      <li>Flat pack assembly</li>
-    </ul>
-
-    <p>
-      Servicing the local Beaudesert and surrounding areas
-    </p>
+  <div>
+    <HeadingSection />
+    <!-- <ContactForm /> -->
   </div>
 </template>
