389ccaaa4a369f2aed0490516bd9ba437a945f46
Damien Robinson <damien.robinson@pocketpasta.com>
2022-02-20 06:35:09 +1000

    fix(gallery): mobile full width

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -2,7 +2,7 @@
   <section class="overflow-hidden text-gray-700 ">
     <div class="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
       <div class="flex flex-wrap -m-1 md:-m-2">
-        <div v-for="item in gallery" :key="item" class="flex flex-wrap w-1/3">
+        <div v-for="item in gallery" :key="item" class="flex flex-wrap md:w-1/3 sm:w-full">
           <div class="w-full p-1 md:p-2">
             <img
               alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
