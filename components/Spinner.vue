5bb6d41869514979d074776339d8a4b9b7cbc526
Damien Robinson <damien.robinson@pocketpasta.com>
2022-02-19 15:23:45 +1000

    Revert "feat: contact form (#4)" (#5)
    This reverts commit 89f0bf4a793f352cdb82519c66f43267aaadab4e.

--- components/Spinner.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div
-    class="spinner-border animate-spin inline-block w-16 h-16 border-4 rounded-full" role="status" style="  vertical-align: -0.125em;
-  border: 0.25em solid;
-  border-right-color: transparent;"
-  >
-    <span class="sr-only">Loading...</span>
-  </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <HeadingSection />
-    <ContactForm />
+  <div class="text-center">
+    <h1 class="text-2xl font-bold leading-7 sm:text-3xl sm:truncate">
+      {{ $config.APP_NAME }}
+    </h1>
+
+    <ul class="list-none">
+      <li>Property maintenance</li>
+      <li>Lawn mowing</li>
+      <li>General handyman services</li>
+      <li>Odd jobs</li>
+      <li>Flat pack assembly</li>
+    </ul>
+
+    <p>
+      Servicing the local Beaudesert and surrounding areas
+    </p>
   </div>
 </template>
