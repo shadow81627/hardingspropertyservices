359617746ee2ebb035f5ab4db3904847fcec5ef0
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-01 12:05:31 +1000

    feat: strapi

--- pages/index.vue
+++ pages/index.vue
@@ -5,3 +5,9 @@
     <ContactForm class="mt-24" />
   </div>
 </template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig()
+const { data } = await useAsyncData('content', () => $fetch(`${config.STRAPI_URL}/api/home?populate=meta`))
+console.log('response', JSON.parse(JSON.stringify(data.value)))
+</script>
