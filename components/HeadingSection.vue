e5001ac348320932045edf2f65dd3953bd1cbc84
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-05 15:33:17 +1000

    feat: about section

--- components/HeadingSection.vue
+++ components/HeadingSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="mb-5">
+  <section>
     <div class="text-center py-24 px-6">
       <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">
         Harding's
@@ -7,18 +7,6 @@
       </h1>
       <!-- <a class="inline-block px-7 py-3 mr-2 bg-green-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Get started</a>
       <a class="inline-block px-7 py-3 bg-transparent text-green-600 font-medium text-sm leading-snug uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Learn more</a> -->
-
-      <ul class="list-none">
-        <li>Property maintenance</li>
-        <li>Lawn mowing</li>
-        <li>General handyman services</li>
-        <li>Odd jobs</li>
-        <li>Flat pack assembly</li>
-      </ul>
-
-      <p>
-        Servicing the local Beaudesert and surrounding areas
-      </p>
     </div>
   </section>
 </template>
