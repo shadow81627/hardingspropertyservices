b99652b68b10feac3e92556ef89f48319a81ad14
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-05 15:34:16 +1000

    feat: title and description from strapi

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
+    <Title v-if="content?.data?.attributes?.meta?.title">
+      {{ content?.data?.attributes?.meta?.title }}
+    </Title>
+    <Meta v-if="content?.data?.attributes?.meta?.description" name="description" :content="content?.data?.attributes?.meta?.description" />
     <HeadingSection />
+    <About class="mb-24" />
     <Gallery />
     <ContactForm class="mt-24" />
   </div>
 </template>
 
-<script setup lang="ts">
-const config = useRuntimeConfig()
-const { data } = await useAsyncData('content', () => $fetch(`${config.STRAPI_URL}/api/home?populate=meta`))
-console.log('response', JSON.parse(JSON.stringify(data.value)))
+<script lang="ts">
+import type { Strapi4Response } from '@nuxtjs/strapi'
+export default {
+  async setup() {
+    const config = useRuntimeConfig()
+    const { data: content } = await useFetch<Strapi4Response>('/api/home', { baseURL: config.STRAPI_URL, params: { populate: 'meta' } })
+    return {
+      content,
+    }
+  },
+}
 </script>
