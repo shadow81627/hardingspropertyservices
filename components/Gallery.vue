e189866b0fb24cc86e4c469e95fe6c500b75f460
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-06 13:53:44 +1000

    feat: gallery page

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -2,8 +2,8 @@
   <section class="overflow-hidden text-gray-700 ">
     <div class="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
       <div class="flex flex-wrap -m-1 md:-m-2">
-        <div v-for="{src, title} in gallery" :key="src" class="flex flex-wrap md:w-1/3 sm:w-full">
-          <div class="w-full m-1 md:m-2" relative overflow-hidden rounded-lg>
+        <div v-for="{src, title, url} in gallery" :key="src" class="flex flex-wrap md:w-1/3 sm:w-full">
+          <Component :is="url ? 'NuxtLink' : 'div'" :to="url" class="w-full m-1 md:m-2" relative overflow-hidden rounded-lg>
             <img
               class="block object-cover object-center w-full h-full"
               :src="src"
@@ -12,7 +12,7 @@
             <div v-if="title" class="absolute w-full py-4 bottom-0 inset-x-0 bg-gray-500 text-center leading-4" text-lg opacity-75 text-white>
               {{ title }}
             </div>
-          </div>
+          </Component>
         </div>
       </div>
     </div>
@@ -24,19 +24,8 @@ export default {
   props: {
     gallery: {
       type: Array,
-      default: () => [
-        { src: '/img/AF1QipM6lEqzYS3JTm-Em6ulliuclqJZ5uE1oh5mUcL_=s1524-k-no.jpg' },
-        { src: '/img/AF1QipMe5BXTXxVqljn7InTndt6x5OMgaD7ftRVcOhwS=s1218-k-no.jpg', title: 'Sunroom reno' },
-        { src: '/img/AF1QipMYbD_Vlj-Fz8bvkg9HXVQ4WomDK7r8bcLDMWFQ=s2031-k-no.jpg', title: 'Lounge room paint refresh' },
-        { src: '/img/AF1QipOfJPUO5qd7kLqeYyDb4aX4cgsefMjZS6cNVvTW=s2031-k-no.jpg', title: 'Lounge room paint refresh' },
-        { src: '/img/AF1QipOR5Y_RoJ-rYAHP-flIVXSJW9cW6oSa9W5xQRgB=s1352-k-no.jpg', title: 'Lounge room paint refresh' },
-        { src: '/img/AF1QipOSCCd7uOTJ36h2BG17XQaOuwqvqsUE-jHQUkF3=s1352-k-no.jpg', title: 'Lounge room paint refresh' },
-        { src: '/img/AF1QipP0e0BPg9clQU6UcTfHihp2N_ifzkbCkT69HN_4=s1218-k-no.jpg', title: 'Sunroom reno' },
-        { src: '/img/AF1QipPiYkY-IgxdCEYDcV6E-0ZdvqU5J1SBcmXaKvUi=s1160-k-no.jpg', title: 'Lounge room paint refresh' },
-        { src: '/img/AF1QipPWRDAdAc_lWJ-MiA8tSn6fGlufrKNPJ052LQnQ=s1218-k-no.jpg', title: 'Sunroom reno' },
-        { src: '/img/AF1QipPKeZSKmlR_l8PyC17JidbZ5S2stcS1mano-TMo=s1218-k-no.jpg', title: 'Lounge room paint refresh' },
-        { src: '/img/AF1QipMJt2hHWj5NN2rucO-w3g7FbrbJJR_aymkeGsjB=s1218-k-no.jpg', title: 'Lounge room paint refresh' },
-      ],
+      default: () => [],
+      required: true,
     },
   },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@
       {{ content?.data?.attributes?.meta?.title }}
     </Title>
     <Meta v-if="content?.data?.attributes?.meta?.description" name="description" :content="content?.data?.attributes?.meta?.description" />
-    <HeadingSection />
+    <HeadingSection first="Harding's" second="Property Services" />
     <About class="mb-24" />
-    <Gallery />
+    <Gallery :gallery="gallery" />
     <ContactForm class="mt-24" />
   </div>
 </template>
@@ -17,8 +17,14 @@ export default {
   async setup() {
     const config = useRuntimeConfig()
     const { data: content } = await useFetch<Strapi4Response>('/api/home', { baseURL: config.STRAPI_URL, params: { populate: 'meta' } })
+    const gallery = [
+      { src: '/img/AF1QipM6lEqzYS3JTm-Em6ulliuclqJZ5uE1oh5mUcL_=s1524-k-no.jpg' },
+      { src: '/img/AF1QipMYbD_Vlj-Fz8bvkg9HXVQ4WomDK7r8bcLDMWFQ=s2031-k-no.jpg', title: 'Lounge room paint refresh', url: '/gallery/lounge-room-paint-refresh' },
+      { src: '/img/AF1QipP0e0BPg9clQU6UcTfHihp2N_ifzkbCkT69HN_4=s1218-k-no.jpg', title: 'Sunroom reno', url: '/gallery/sunroom-reno' },
+    ]
     return {
       content,
+      gallery,
     }
   },
 }
