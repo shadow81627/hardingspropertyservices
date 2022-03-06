31be4cb913d3a9bba9843d2b8fd391575f8b4ec8
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-06 16:42:13 +1000

    refactor(app): setup function

--- app.vue
+++ app.vue
@@ -20,69 +20,73 @@
   </Html>
 </template>
 
-<script setup>
-const config = useRuntimeConfig()
-const jsonLD = JSON.stringify({
-  '@context': 'https://schema.org/',
-  '@type': 'Service',
-  serviceType: 'Handyman',
-  provider: {
-    '@type': 'LocalBusiness',
-    name: config.APP_NAME,
-    // 'telephone': '0487 950 940',
-    url: 'https://hardingspropertyservices.com.au',
-    sameAs: [
-      'https://www.instagram.com/hardingspropertyservices/',
-      'https://www.facebook.com/hardingspropertyservices',
-    ],
+<script lang="ts">
+export default {
+  setup() {
+    const config = useRuntimeConfig()
+    const jsonLD = JSON.stringify({
+      '@context': 'https://schema.org/',
+      '@type': 'Service',
+      serviceType: 'Handyman',
+      provider: {
+        '@type': 'LocalBusiness',
+        name: config.APP_NAME,
+        // 'telephone': '0487 950 940',
+        url: 'https://hardingspropertyservices.com.au',
+        sameAs: [
+          'https://www.instagram.com/hardingspropertyservices/',
+          'https://www.facebook.com/hardingspropertyservices',
+        ],
+      },
+      areaServed: {
+        '@type': 'City',
+        name: ['Beaudesert', 'Kooralbyn'],
+      },
+      hasOfferCatalog: {
+        '@type': 'OfferCatalog',
+        name: 'Property services',
+        itemListElement: [
+          {
+            '@type': 'Offer',
+            itemOffered: {
+              '@type': 'Service',
+              name: 'Property maintenance',
+            },
+          },
+          {
+            '@type': 'Offer',
+            itemOffered: {
+              '@type': 'Service',
+              name: 'Lawn mowing',
+            },
+          },
+          {
+            '@type': 'Offer',
+            itemOffered: {
+              '@type': 'Service',
+              name: 'General handyman services',
+            },
+          },
+          {
+            '@type': 'Offer',
+            itemOffered: {
+              '@type': 'Service',
+              name: 'Odd jobs',
+            },
+          },
+          {
+            '@type': 'Offer',
+            itemOffered: {
+              '@type': 'Service',
+              name: 'Flat pack assembly',
+            },
+          },
+        ],
+      },
+    })
+    return jsonLD
   },
-  areaServed: {
-    '@type': 'City',
-    name: ['Beaudesert', 'Kooralbyn'],
-  },
-  hasOfferCatalog: {
-    '@type': 'OfferCatalog',
-    name: 'Property services',
-    itemListElement: [
-      {
-        '@type': 'Offer',
-        itemOffered: {
-          '@type': 'Service',
-          name: 'Property maintenance',
-        },
-      },
-      {
-        '@type': 'Offer',
-        itemOffered: {
-          '@type': 'Service',
-          name: 'Lawn mowing',
-        },
-      },
-      {
-        '@type': 'Offer',
-        itemOffered: {
-          '@type': 'Service',
-          name: 'General handyman services',
-        },
-      },
-      {
-        '@type': 'Offer',
-        itemOffered: {
-          '@type': 'Service',
-          name: 'Odd jobs',
-        },
-      },
-      {
-        '@type': 'Offer',
-        itemOffered: {
-          '@type': 'Service',
-          name: 'Flat pack assembly',
-        },
-      },
-    ],
-  },
-})
-return jsonLD
+}
 </script>
 
 <style>
