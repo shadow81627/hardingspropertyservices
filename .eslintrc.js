c401c1d0baefbc2b7cd39fc84db50306b2e67bd7
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-06 17:53:25 +1000

    chore(eslint): plugin prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,17 @@ module.exports = {
   },
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:nuxt/recommended',
     'plugin:vue/vue3-recommended',
+    'prettier',
     'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2021,
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
-  plugins: ['vue', '@typescript-eslint'],
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
   rules: {
     'vue/multi-word-component-names': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
