fd489d032eadde360cb165323db7f746a643536b
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-06 15:25:57 +1000

    fix(contact section): desktop margin 0

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -9,6 +9,7 @@
         to="/contact"
         ml-auto
         my-4
+        md:my-0
         class="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-lg leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out"
       >
         Contact Us
