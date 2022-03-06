e88c2b5eeac06368e26fca67b7c2f8e19f390943
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-06 13:52:26 +1000

    feat(navigation): breadcrumbs

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,35 @@
+<template>
+  <ol class="list-reset flex">
+    <li><a href="/" class="text-gray-500 hover:text-gray-600">Home</a></li>
+    <template v-for="({title, url}, index) in items" :key="url">
+      <li v-if="index != (items.lenght - 1)">
+        <span class="text-gray-500 mx-2">/</span>
+      </li>
+      <li>
+        <NuxtLink :to="url" class="text-gray-500 hover:text-gray-600">
+          {{ title }}
+        </NuxtLink>
+      </li>
+    </template>
+  </ol>
+</template>
+
+<script>
+export default {
+  computed: {
+    items() {
+      const route = useRoute()
+      const router = useRouter()
+      return useSplit(route.path, '/')
+        .filter(Boolean)
+        .map(function(item) {
+          this.acc = `${this.acc}/${item}`
+          return { title: useStartCase(item), url: this.acc }
+        // https://stackoverflow.com/a/47968178
+        }, { acc: '' })
+      // https://dev.to/lukeocodes/breadcrumbs-in-nuxt-5f2m
+        .filter(({ url }) => router.resolve(url).name !== null)
+    },
+  },
+}
+</script>
--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,7 +2,7 @@
   <nav class="relative w-full flex flex-wrap items-center justify-between py-3">
     <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
       <div class="container-fluid">
-            &nbsp;
+        <Breadcrumb />
       </div>
       <DarkToggle />
     </div>
