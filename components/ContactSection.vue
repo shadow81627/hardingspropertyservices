a47fde8c08651b5a0a1a674c73886f8d2786a50b
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-06 15:25:05 +1000

    fix(contact section): mobile style

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -1,14 +1,15 @@
 <template>
   <section container px-5 py-2 mx-auto lg:pt-12 lg:px-32>
-    <div class="p-16 rounded-lg bg-gray-100 text-gray-700" flex flex-row>
+    <div class="p-8 rounded-lg bg-gray-100 text-gray-700" flex md:flex-row flex-col>
       <h2 class="font-semibold text-3xl">
-        Have Enquiry? Don’t hesitate to use the contact form.
+        Have an Enquiry?
       </h2>
       <NuxtLink
         type="button"
         to="/contact"
         ml-auto
-        class="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out"
+        my-4
+        class="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-lg leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out"
       >
         Contact Us
       </NuxtLink>
