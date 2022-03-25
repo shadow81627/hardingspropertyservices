f1c128b59e7760790edd4b1b3de6ffadc7e2195a
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-25 15:30:53 +1000

    fix(home): right quote in heading

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       name="description"
       :content="content?.description"
     />
-    <HeadingSection first="Harding's" second="Property Services">
+    <HeadingSection first="Harding&rsquo;s" second="Property Services">
       <NuxtLink
         class="inline-block px-7 py-3 mr-2 bg-green-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out"
         to="/contact"
