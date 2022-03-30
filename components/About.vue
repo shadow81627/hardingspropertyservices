475f199f8644983e0d23676f3addef5c87b6ef9d
Damien Robinson <damien.robinson@pocketpasta.com>
2022-03-30 12:57:19 +1000

    feat: about strapi fetch

--- components/About.vue
+++ components/About.vue
@@ -14,46 +14,74 @@
       bg-gray-100
       text-gray-700
       bg-opacity-75
-
       dark:bg-gray-900
       dark:text-gray-300
       dark:bg-opacity-75
     >
-      <p>
-        Wayne Harding has a lifelong passion for home improvement and
-        landscaping. He enjoys doing reno, gardening and DIY projects in his own
-        home, and has worked professionally in this area for over 30 years.
-        After decades of working these various building, mowing, turfing and
-        hardware jobs, he has decided to use this knowledge to branch out and
-        grow his own business. Wayne provides a range of property services to
-        Beaudesert and surrounding areas:
-      </p>
+      <div v-if="content" v-html="content.paragraph1"></div>
 
-      <ul list-disc list-inside py-2 flex justify-between flex-wrap>
-        <li md:px-2 py-2 flex-auto w-full md:w-auto>Property maintenance</li>
-        <li md:px-2 py-2 flex-auto w-full md:w-auto>Lawn mowing</li>
-        <li md:px-2 py-2 flex-auto w-full md:w-auto>
-          General handyman services
-        </li>
-        <li md:px-2 py-2 flex-auto w-full md:w-auto>Odd jobs</li>
-        <li md:px-2 py-2 flex-auto w-full md:w-auto>Flat pack assembly</li>
-        <li md:px-2 py-2 flex-auto w-full md:w-auto>Gutter Cleaning</li>
-        <li md:px-2 py-2 flex-auto w-full md:w-auto>Rubbish jobs</li>
-      </ul>
-
-      <p>
-        If you&rsquo;re in need of these services in your home, or you have an
-        enquiry about a different odd job that needs doing, don&rsquo;t hesitate
-        to use the
-        <NuxtLink
-          to="/contact"
-          class="text-primary-600 hover:text-primary-700 transition duration-300 ease-in-out"
-          dark:text-white-600 dark:hover:text-white-700
-          font-bold
+      <ul
+        list-disc
+        list-inside
+        py-2
+        flex
+        justify-between
+        flex-wrap
+        v-if="content"
+      >
+        <li
+          md:px-2
+          py-2
+          flex-auto
+          w-full
+          md:w-auto
+          v-for="service of content.services"
+          :key="service.name"
         >
-          contact form on our website </NuxtLink
-        >. We will get back to you with a quote as soon as we can.
-      </p>
+          {{ service.name }}
+        </li>
+      </ul>
+      <div v-if="content" v-html="content.paragraph2"></div>
     </div>
   </section>
 </template>
+
+<script lang="ts">
+import type { Strapi4Response } from '@nuxtjs/strapi'
+import { marked } from 'marked'
+export default {
+  async setup() {
+    const config = useRuntimeConfig()
+    function transform(content: Strapi4Response) {
+      try {
+        const paragraph1 = marked.parse(content?.data?.attributes?.paragraph1)
+        const paragraph2 = marked.parse(content?.data?.attributes?.paragraph2)
+        const services = content?.data?.attributes?.services?.data.map(
+          ({ attributes: { name } }) => ({
+            name,
+          }),
+        )
+        return {
+          paragraph1,
+          paragraph2,
+          services,
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    const { data: content } = await useFetch<Strapi4Response>('/api/about', {
+      baseURL: config.STRAPI_URL,
+      params: {
+        'populate[0]': 'services',
+        t: 1648607183585,
+      },
+      server: true,
+      transform,
+    })
+    return {
+      content: content ?? {},
+    }
+  },
+}
+</script>
