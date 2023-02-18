c586aeb927eab6af5589e1cac322d33ac25f45bd
Damien Robinson <damien.robinson@daim.dev>
2023-02-18 12:01:30 +1000

    fix(gallery): add img classes

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -25,6 +25,8 @@
               height="555"
               sizes="sm:100vw md:33vw lg:416px"
               itemprop="contentUrl"
+              loading="lazy"
+              :imgAttrs="{class: 'block object-fit object-center w-full h-full'}"
             />
             <div
               v-if="title"
