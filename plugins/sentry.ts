507e146e9979d26698faa4530f14f9d8308ede31
Damien Robinson <damien.robinson@daim.dev>
2023-02-18 13:05:37 +1000

    style: lint fix

--- plugins/sentry.ts
+++ plugins/sentry.ts
@@ -1,12 +1,12 @@
 // import Toucan from 'toucan-js';
 // import { createRouter } from "vue-router";
-import * as Sentry from "@sentry/vue";
+import * as Sentry from '@sentry/vue'
 // import { BrowserTracing } from "@sentry/tracing";
 
 export default defineNuxtPlugin((nuxtApp) => {
   Sentry.init({
     app: nuxtApp.vueApp,
-    dsn: "https://980405b334ef4025bb2cee8da9787b61@o239887.ingest.sentry.io/6268262",
+    dsn: 'https://980405b334ef4025bb2cee8da9787b61@o239887.ingest.sentry.io/6268262',
     // integrations: [
     //   new BrowserTracing({
     //     routingInstrumentation: Sentry.vueRouterInstrumentation(router),
@@ -16,5 +16,5 @@ export default defineNuxtPlugin((nuxtApp) => {
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
-  });
+  })
 })
