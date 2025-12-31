46ffc0a559d82a5b4f044da4f29f2f4967e1feff
Damien Robinson <damien.robinson@daim.dev>
2025-12-31 11:57:00 +1000

    fix(contact): close enquiries

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -9,9 +9,17 @@
     text-center
     md:text-left
   >
-    <div class="p-8" flex md:flex-row flex-col section-bg>
-      <h2 class="font-semibold text-3xl">Have an Enquiry?</h2>
-      <NuxtLink
+    <div
+      class="p-8 justify-between items-center"
+      flex
+      md:flex-row
+      flex-col
+      section-bg
+    >
+      <h2 class="font-semibold text-3xl">
+        Sorry, we aren't taking enquiries at the moment.
+      </h2>
+      <!-- <NuxtLink
         type="button"
         to="/contact"
         ml-auto
@@ -20,7 +28,7 @@
         class="inline-block px-6 py-2.5 bg-primary text-white font-medium text-lg leading-tight uppercase rounded shadow-md hover:bg-primary hover:shadow-lg focus:bg-primary focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary active:shadow-lg transition duration-150 ease-in-out"
       >
         Contact Us
-      </NuxtLink>
+      </NuxtLink> -->
     </div>
   </section>
 </template>
--- pages/contact.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <Html itemscope itemtype="https://schema.org/ContactPage" />
-    <HeadingSection first="Contact" />
-    <ContactForm />
-  </div>
-</template>
