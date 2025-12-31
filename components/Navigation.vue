9a2e086b9e16880badabb3e6445d20d5da1379fb
Damien Robinson <damien.robinson@daim.dev>
2025-12-31 11:57:54 +1000

    fix(navigation): replace links with logo

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -7,8 +7,8 @@
     <div
       class="container-fluid w-full flex flex-wrap items-center justify-between px-6"
     >
-      <div class="container-fluid" flex justify-between h-16 lg:space-x-8>
-        <Links />
+      <div class="container-fluid py-3" flex justify-between h-16 lg:space-x-8>
+        <img src="/hardings-property.svg" />
       </div>
       <div flex justify-between>
         <span inline-flex items-center p-2>ABN 53 351 269 695</span>
