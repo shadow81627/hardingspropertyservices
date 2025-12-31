129555c968c8230cb7b6cd95031c78df37dce589
Damien Robinson <damien.robinson@daim.dev>
2025-12-31 12:24:12 +1000

    fix(header navigation): remove abn to stop stacking thick header

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -8,11 +8,15 @@
       class="container-fluid w-full flex flex-wrap items-center justify-between px-6"
     >
       <div class="container-fluid py-3" flex justify-between h-16 lg:space-x-8>
-        <img src="/hardings-property.svg" />
+        <img
+          src="/hardings-property.svg"
+          height="40"
+          alt="Harding's Property Services"
+        />
       </div>
-      <div flex justify-between>
+      <!-- <div md:flex justify-between hidden>
         <span inline-flex items-center p-2>ABN 53 351 269 695</span>
-      </div>
+      </div> -->
     </div>
   </nav>
 </template>
