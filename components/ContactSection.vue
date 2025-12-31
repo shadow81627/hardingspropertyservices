57ac616a232cabae21dcf5c40e29d3ab648c9158
Damien Robinson <damien.robinson@daim.dev>
2025-12-31 12:11:37 +1000

    fix(contact section): add closed books message from facebook

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -13,12 +13,23 @@
       class="p-8 justify-between items-center"
       flex
       md:flex-row
+      gap-8
       flex-col
       section-bg
     >
-      <h2 class="font-semibold text-3xl">
-        Sorry, we aren't taking enquiries at the moment.
-      </h2>
+      <h2 class="font-semibold text-3xl">Books are closed</h2>
+      <div class="w-[65ch] text-pretty flex flex-col gap-4">
+        <p>
+          Thank you to everyone for your recent enquiries. Unfortunately
+          Harding's Property Services can not take on any more work for the
+          foreseeable future.
+        </p>
+        <p>
+          Our books are now closed for new clients. Thank you for your support
+          and understanding.
+        </p>
+      </div>
+
       <!-- <NuxtLink
         type="button"
         to="/contact"
